53cdad8bd124bc36ca7849b6902f97e99f5d55d6
vishnu <vishnuvardhankalva8@gmail.com>
2024-08-26 19:38:58 -0400

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+import logger
+
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()  ## exc_tb == traceback i will store on which line number the exception is occurs.... whic file 
+    
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    error_message = "Error occeured in python script name [{0}] Line number [{1}], error message [{2}]".format(file_name,line_number,str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__()   ## Initialize the parent Excepting class
+        self.error_message = error_message_details(error_message,error_detail=error_detail)
+    
+    def __str__(self):  # This method return the detailed error message when the exceptin is printed or converted to a string
+        return self.error_message
+
+
